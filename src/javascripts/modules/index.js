b5868962965767883c0ae1bb252f7bf2cb6fd5d4
Chad Altemose <chad@chadzilla.com>
2016-10-27 06:58:41 -0400

    cleanup, unused file removal

--- src/javascripts/modules/index.js
+++ src/javascripts/modules/index.js
@@ -3,39 +3,5 @@ import TravelStories from './travel-stories';
 const jsonFile = '/travel-stories.json';
 let el = document.getElementById('content');
 
-
 let app = new TravelStories(el);
 app.init(jsonFile);
-
-
-/*
-  Automatically instantiates modules based on data-attrubiutes
-  specifying module file-names.
-*/
-
-// const moduleElements = document.querySelectorAll('[data-module]')
-//
-// for (var i = 0; i < moduleElements.length; i++) {
-//   const el = moduleElements[i]
-//   const name = el.getAttribute('data-module')
-//   const Module = require(`./${name}`).default
-//   new Module(el)
-// }
-
-/*
-  Usage:
-  ======
-
-  html
-  ----
-  <button data-module="disappear">disappear!</button>
-
-  js
-  --
-  // modules/disappear.js
-  export default class Disappear {
-    constructor(el) {
-      el.style.display = none
-    }
-  }
-*/
