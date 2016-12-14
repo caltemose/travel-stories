42d720400ece321896e1ac4eac7091951d23873a
Chad Altemose <chad@chadzilla.com>
2016-12-13 22:01:01 -0500

    added ischia-monteferrante slideshow and GET query parameter functionality

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,9 +1,19 @@
 import TravelStories from './modules/TravelStories'
 
 let content = document.getElementById('content')
-let json = './italy-highlights.json'
+
+// default file to load
+let json = './italy.json'
+
+// can be overridden by html data attribute
 if (content.dataset.jsonSrc) {
     json = content.dataset.jsonSrc
+}
+
+// which can be overridden by GET parameters
+var urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get('slideshow') || urlParams.get('s')) {
+    json = urlParams.get('slideshow') ? urlParams.get('slideshow') : urlParams.get('s')
 }
 
 let controls = document.getElementById('controls')
